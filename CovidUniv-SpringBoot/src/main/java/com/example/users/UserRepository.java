c8ae99e73554e7e52eabb1c85b761ba3aef86eb5
ghada097 <66436902+ghada097@users.noreply.github.com>
2021-04-09 20:04:08 +0100

    testing commit

--- CovidUniv-SpringBoot/src/main/java/com/example/users/UserRepository.java
+++ CovidUniv-SpringBoot/src/main/java/com/example/users/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.users;
 import com.example.users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// test
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
